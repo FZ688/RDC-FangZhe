6899fb787fa5f4de81590c35f010acdc527ce140
FZ688 <2307839919@qq.com>
2024-01-29 23:32:50 +0800

    添加README

--- Train/src/week2/Demo.java
+++ Train/src/week2/Demo.java
@@ -1,17 +1,22 @@
 package week2;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * @author fz
  */
 public class Demo {
     public static void main(String[] args) {
-       /* 假设X=30,N=3,M=5;
-        一周内吃超过X元的食物不能超过N次；
-        一周内吃重复食物的次数不能超过M次
+       /*
+        假设X=30,N=3,M=5;
+        一周内吃超过X元的食物不能超过N次;
+        一周内吃重复食物的次数不能超过M次;
         */
         List<Food> foods = new ArrayList<>();
         Food food1 = new Food( "牛肉面", 20.6);
@@ -23,32 +28,36 @@ public class Demo {
         Collections.addAll(foods,food1,food2,food3,food4,food5,food6);
 
 
-        /*用Map集合的值来统计吃的次数
-        * 用List集合来存入生成一周午餐和晚餐的食物
-        * */
+        /*
+          用Map集合的值来统计吃的次数
+          用List集合来存入生成一周午餐和晚餐的食物
+         */
         Map<Food,Integer> statistic = new HashMap<>();
         List<Food> oneWeekFood =  new ArrayList<>();
         Random r = new Random();
         for (int i = 0; i < 14; i++) {
             //随机数决定吃什么
             int index = r.nextInt(foods.size());
-            if (statistic.get(foods.get(index)) == null){
-                statistic.put(foods.get(index),1);
-                oneWeekFood.add(foods.get(index));
+            Food randomFood = foods.get(index);
+            if (statistic.get(randomFood) == null){
+                statistic.put(randomFood,1);
+                oneWeekFood.add(randomFood);
             }else {
                 //一周内吃重复食物的次数不能超过5次
                 // 价格超过30元的所有食物的次数不能超过3次
-                while (statistic.get(foods.get(index)) >= 5 ||
-                        (statistic.containsKey(foods.get(1))&&statistic.containsKey(foods.get(3))&&foods.get(index).getPrice() > 30 && statistic.get(foods.get(1))+statistic.get(foods.get(3))>3)){
+                while (statistic.get(randomFood) >= 5 ||
+                        (foods.get(index).getPrice() > 30 && priceBeyond30(oneWeekFood)>=3)){
                     index = r.nextInt(foods.size());
+                    randomFood = foods.get(index);
                 }
-                statistic.put(foods.get(index),statistic.get(foods.get(index))+1);
-                oneWeekFood.add(foods.get(index));
+                statistic.put(randomFood,statistic.get(randomFood)+1);
+                oneWeekFood.add(randomFood);
             }
         }
 
-        /*将装有一周食物的List集合里的元素分发给7天的集合
-        * */
+        /*
+         将装有一周食物的List集合里的元素分发给7天的集合
+        */
         List<Food> mondayFood = new ArrayList<>();
         List<Food> tuesdayFood = new ArrayList<>();
         List<Food> wednesdayFood = new ArrayList<>();
@@ -86,9 +95,10 @@ public class Demo {
                 }
             }
         }
+
         /*
-        * 打算搞个集合嵌套创建那张食物清单
-        * */
+        打算搞个集合嵌套创建那张食物清单
+        */
         Map<String,List<Food>> foodList = new LinkedHashMap<>();
         foodList.put("周一",mondayFood);
         foodList.put("周二",tuesdayFood);
@@ -98,23 +108,37 @@ public class Demo {
         foodList.put("周六",saturdayFood);
         foodList.put("周日",sundayFood);
 
+        //打印清单
         foodList.forEach((d,f)->{
             System.out.println(d+"："+toString(f));
         });
 
     }
 
-
-    /*自己懒得想模板，就按师兄的来
-    * */
-    public static String toString(List<Food> foods){
-        StringBuffer strBack = new StringBuffer();
-        for (Food food : foods) {
-            String str = food.getName()+"："+food.getPrice()+"元  ";
-            strBack.append(str);
+        /**
+         *统计超过价格超过30元的食物的数量
+         */
+        public static int priceBeyond30(List<Food> foods){
+            int count = 0;
+            for (Food food : foods) {
+                if (food.getPrice() > 30){
+                    count++;
+                }
+            }
+            return count;
         }
-        return strBack.toString();
-    }
+
+        /**
+        * 自己懒得想模板，就按师兄的来
+        */
+        public static String toString(List<Food> foods){
+            StringBuffer strBack = new StringBuffer();
+            for (Food food : foods) {
+                String str = food.getName()+"："+food.getPrice()+"元  ";
+                strBack.append(str);
+            }
+            return strBack.toString();
+        }
 
 
 }
